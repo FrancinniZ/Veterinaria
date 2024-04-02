327a0e29e4e10c9b276e98d65c07774a0c496e4d
Lenuxgg <manuellenalvarado@gmail.com>
2024-04-01 20:17:20 -0600

    Cambie el css de le nav

--- sistema/includes/nav.php
+++ sistema/includes/nav.php
@@ -1,17 +1,61 @@
 <nav>
+	<ul>
+		<a href="#">
+			<img class="photoinicio" src="https://cdn-icons-png.flaticon.com/512/403/403890.png" alt="Logopic">
+		</a>
+
+		<li><a href="#">Inicio</a></li>
+		<!-- <li class="principal">
+			<a href="#">Usuarios</a>
 			<ul>
-				<li><a href="#">Principal</a></li>
-				<li class="principal">
-					<a href="#">Usuarios</a>
-				</li>
-				<li class="principal">
-					<a href="#">Adopciones</a>
-				</li>
-				<li class="principal">
-					<a href="#">Citas</a>
-				</li>
-				<li class="principal">
-					<a href="#">Reportes</a>
-				</li>
+				<li><a href="#">Nuevo Usuario</a></li>
+				<li><a href="#">Lista de Usuarios</a></li>
 			</ul>
-		</nav>
+		</li> -->
+		<li class="principal">
+			<a href="conozcanos.php">Conozcanos</a>
+		</li>
+		<li class="principal">
+			<a href="#">Servicios</a>
+			<ul>
+				<li><a href="servicioMedico.php">Servicio Medico</a></li>
+				<li><a href="cuidadoya.php">Cuidado y aseo</a></li>
+				<li><a href="dermatologia.php">Dermatología</a></li>
+				<li><a href="otorrino.php">otorrinolaringología</a></li>
+				<li><a href="cirugia.php">Cirugía</a></li>
+			</ul>
+		</li>
+		<li class="principal">
+			<a href="#">Adopciones</a>
+			<!-- <ul>
+				<li><a href="#">Nueva Adopción</a></li>
+				<li><a href="#">Lista de Adopciones</a></li>
+			</ul> -->
+		</li>
+		<li class="principal">
+			<a href="#">Citas</a>
+			<!-- <ul>
+				<li><a href="#">Nueva Cita</a></li>
+				<li><a href="#">Lista de Citas</a></li>
+			</ul> -->
+		</li>
+		<li class="principal">
+			<a href="#">Reportes</a>
+			<ul>
+				<li><a href="#">Nuevo Reporte</a></li>
+				<!-- <li><a href="#">Reporte</a></li> -->
+			</ul>
+		</li>
+		<li class="principal">
+			<a href="#">Contactenos</a>
+		</li>
+		<div class="optionsBar">
+			<span class="user"></span>
+			<img class="photouser" src="img/iconavatar.png" alt="Usuario">
+			<a href="salir.php">
+				<img class="close" src="img/Iconexit.png" alt="Salir del sistema" title="Salir">
+			</a>
+		</div>
+	</ul>
+
+</nav>
