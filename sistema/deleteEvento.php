7955fcf5591e88e09927e63cddf9930ba20f4f45
Fafa0516 <134663051+Fafa0516@users.noreply.github.com>
2024-04-21 21:49:57 -0600

    Delete sistema/deleteEvento.php

--- sistema/deleteEvento.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('bd/conexion.php');
-$id    		= $_REQUEST['id']; 
-
-$sqlDeleteEvento = ("DELETE FROM calendario WHERE  id='" .$id. "'");
-$resultProd = mysqli_query($con, $sqlDeleteEvento);
-
-?>
-  
