b36ccdc75ecd0551ff65e5478910b729a6082e0f
RoblesGabriela <68611743+RoblesGabriela@users.noreply.github.com>
2024-04-21 21:47:34 -0600

    Delete sistema/bd/conexion.php

--- sistema/bd/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$usuario  = "root";
-$password = "";
-$servidor = "localhost";
-$basededatos = "veterinaria";
-$con = mysqli_connect($servidor, $usuario, $password) or die("Sin conexion al servidor");
-$db = mysqli_select_db($con, $basededatos) or die("Error al conectar a BD");
-?>
-
